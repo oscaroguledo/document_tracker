555681a37ac1bd6cc55090fb87571097b166180e
oscaroguledo <122152636+oscaroguledo@users.noreply.github.com>
2023-11-15 17:16:27 +0100

    started the also like function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
+import re
 import matplotlib.pyplot as plt
 import pycountry
 import pycountry_convert as pc
+from collections import defaultdict
 
 class DataGetter():
     def __init__(self,file_path) -> None:
@@ -35,7 +37,6 @@ class DataGetter():
     def countries_data(self):
         data = {}
         for i in self.data:
-            print(i)
             country = i['visitor_country']
             if country in data.keys():
                 data[country]+=1
@@ -53,15 +54,85 @@ class DataGetter():
             else:
                 data[continent]=1
         return data
+    def __identify_browser(self,user_agent):
+        if re.search(r'Chrome', user_agent):
+            return 'Google Chrome'
+        elif re.search(r'Safari', user_agent) and re.search(r'AppleWebKit', user_agent):
+            return 'Safari'
+        elif re.search(r'Firefox', user_agent):
+            return 'Mozilla Firefox'
+        elif re.search(r'Opera', user_agent):
+            return 'Opera'
+        elif re.search(r'Edge', user_agent):
+            return 'Microsoft Edge'
+        elif re.search(r'MSIE', user_agent):
+            return 'Internet Explorer'
+        else:
+            return 'Unknown Browser'
+
     def browser_data(self):
         data = {}
         for i in self.data:
-            browser = i['visitor useragent']
+            browser = i['visitor_useragent']
+            browser = self.__identify_browser(browser)
             if browser in data.keys():
                 data[browser]+=1
             else:
                 data[browser]=1
         return data
+    def reading_time(self):
+        # Create a dictionary to store total reading time for each user
+        user_reading_time = defaultdict(int)
+
+        # Process JSON data to calculate total reading time for each user
+        
+        for entry in self.data:
+            if entry['event_type'] == 'pagereadtime':
+                user_uuid = entry['visitor_uuid']
+                read_time = entry['event_readtime'] if 'event_readtime' in entry else 0
+                user_reading_time[user_uuid] += read_time
+
+        # Get the top 10 readers based on total reading time
+        top_10_readers = sorted(user_reading_time.items(), key=lambda x: x[1], reverse=True)[:10]
+
+        # Display the top 10 readers and their total reading time
+        data={"title":"Top 10 readers based on total reading time:", 'data':[]}
+        
+        for rank, (user_id, total_time) in enumerate(top_10_readers, start=1):
+            data["data"].append({"rank:":rank,"User_ID:":user_id,"total_reading_time:":f"{total_time} secs"})
+        return data
+    
+    def __get_document_readers(self,doc_id):
+        readers = set()
+        for i in self.data:
+            if 'subject_doc_id' in i.keys():
+                if i['subject_doc_id'] == doc_id:
+                    readers.add(i['visitor_uuid'])
+        return readers
+    def __get_reader_documents(self,visitor_uuid):
+        documents = set()
+        for i in self.data:
+            if 'visitor_uuid' in i.keys():
+                if i['visitor_uuid'] == visitor_uuid:
+                    documents.add(i['env_doc_id'])
+        return documents
+    def also_like(self,doc_id, visitor_uuid=None, sorting_function=None):
+        all_readers = self.__get_document_readers(doc_id)
+        related_docs_count = defaultdict(int)
+
+        for reader in all_readers:
+            related_documents = self.__get_reader_documents(reader)
+            for related_doc in related_documents:
+                if related_doc != doc_id:
+                    related_docs_count[related_doc] += 1
+
+        sorted_related_docs = sorted(related_docs_count.items(), key=lambda x: sorting_function(x[1]), reverse=True)
+
+        if visitor_uuid:
+            visitor_docs = self.__get_reader_documents(visitor_uuid)
+            sorted_related_docs = [doc for doc in sorted_related_docs if doc[0] not in visitor_docs]
+
+        return [doc[0] for doc in sorted_related_docs][:10]
 
 
 if __name__ == "__main__":
@@ -69,5 +140,12 @@ if __name__ == "__main__":
 
     # Load data from the file
     data_getter = DataGetter(file_path)
-    load_data = data_getter.browser_data()
-    print(load_data)
+    """
+    reading_time = data_getter.reading_time()
+    print(reading_time)"""
+    # Example usage:
+    document_id = "140224195414-e5a9acedd5eb6631bb6b39422fba6798"
+    visitor_id = "04daa9ed9dde73d3"
+    # Sort based on the number of readers
+    result = data_getter.also_like(document_id, visitor_id, lambda x: x)
+    print("Top 10 'also liked' documents based on number of readers:", result)
--- test.py
+++ test.py
@@ -1,47 +1,22 @@
-import json
-import matplotlib.pyplot as plt
+import re
 
-def load_data(file_path):
-    with open(file_path, 'r') as file:
-        data={}
-        for f in file:
-            item = json.loads(f)
-            if not item["visitor_country"] in data.keys():
-                data[item["visitor_country"]]=1
-            else:
-                data[item["visitor_country"]]+=1
-        print(data)
-        data = [json.loads(line) for line in file]
-    return data
-class DataGetter():
-    def __init__(self) -> None:
-        pass
-    def load_data(file_path):
-        with open(file_path, 'r') as file:
-            data={}
-            for f in file:
-                item = json.loads(f)
-                if not item["visitor_country"] in data.keys():
-                    data[item["visitor_country"]]=1
-                else:
-                    data[item["visitor_country"]]+=1
-            print(data)
-            data = [json.loads(line) for line in file]
-        return data
+def identify_browser(user_agent):
+    if re.search(r'Chrome', user_agent):
+        return 'Google Chrome'
+    elif re.search(r'Safari', user_agent) and re.search(r'AppleWebKit', user_agent):
+        return 'Safari'
+    elif re.search(r'Firefox', user_agent):
+        return 'Mozilla Firefox'
+    elif re.search(r'Opera', user_agent):
+        return 'Opera'
+    elif re.search(r'Edge', user_agent):
+        return 'Microsoft Edge'
+    elif re.search(r'MSIE', user_agent):
+        return 'Internet Explorer'
+    else:
+        return 'Unknown Browser'
 
-def plot_histogram(data):
-    plt.hist(data, bins=10, color='blue', edgecolor='black', alpha=0.7)
-    plt.xlabel('Values')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of Data')
-    plt.show()
-
-if __name__ == "__main__":
-    # Replace 'data.json' with the actual path to your JSON data file
-    data_file_path = 'sample_tiny.json'
-
-    # Load data from the file
-    numerical_data = load_data(data_file_path)
-
-    # Plot histogram
-    plot_histogram(numerical_data)
+# Example usage:
+user_agent_string = "Mozilla/5.0 (Windows NT 6.1; rv:27.0) Gecko/20100101 Firefox/27.0"
+browser = identify_browser(user_agent_string)
+print(f"The browser used is: {browser}")
